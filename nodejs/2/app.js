75adb30244f42950d68b58cfe7a0e14664bda5d7
Mauro <101263125+PeanutSouth@users.noreply.github.com>
2023-05-23 17:46:11 -0300

    23/05
    anda

--- nodejs/2/app.js
+++ nodejs/2/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 const path =require('path')
@@ -17,3 +16,21 @@ app.get('/carrusel', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'alumno',
+  password : 'alumnoipm',
+  database : 'mydb'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
